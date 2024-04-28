62d1224128cd9bb4e49a69604bcacde2be809ab7
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2024-04-28 12:52:30 +0200

    fix quick in task 6 so todos run in order

--- tasks/6/assembly/index.ts
+++ tasks/6/assembly/index.ts
@@ -1,7 +1,7 @@
 import { consoleLogInJs, logFunctionInJs } from "./log";
 
 export function run(fromJs: string): string {
-  logFunctionInJs("functionInJs: Hello from global function.");
   consoleLogInJs("console.log: Hello from console.log.");
+  logFunctionInJs("functionInJs: Hello from global function.");
   return `${fromJs} is done.`;
 }
